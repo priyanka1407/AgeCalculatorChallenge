bdda3c2f4215cb8db316fc96dd195c92b56f4793
Priyanka0706 <priyankamurugavel@gmail.com>
2021-10-28 06:27:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+total_age_living=90
+
+total_age_left=total_age_living- int(age)
+print(total_age_left)
+
+days_left=total_age_left*365
+weeks_left=total_age_left*52
+months_left=total_age_left*12
+
+print(f"you have {days_left} days,{weeks_left} weeks, and {months_left} months left")
+
+
+
+
+
+
+
